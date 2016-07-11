156048efdeb20457a532d58afe2cd59176178d92
anproagile <anproagile@gmail.com>
2016-07-11 10:49:38 +0800

    test(example): add example native code

--- index.js
+++ index.js
@@ -120,14 +120,7 @@ class DatePicker extends Component {
       this.props.onDateChange(this.getDateStr(this.state.date));
     }
   }
-  
-  getTitle() {
-      if (!this.props.date && this.props.placeholder) {
-          return this.props.placeholder;
-      }
-      return this.getDateStr();
-  }
-  
+
   onDatePicked({action, year, month, day}) {
     if (action !== DatePickerAndroid.dismissedAction) {
       this.setState({
@@ -222,7 +215,7 @@ class DatePicker extends Component {
       >
         <View style={[Style.dateTouchBody, this.customStyles.dateTouchBody]}>
           <View style={[Style.dateInput, this.customStyles.dateInput, this.state.disabled && Style.disabled]}>
-            <Text style={[Style.dateText, this.customStyles.dateText]}>{this.getTitle()}</Text>
+            <Text style={[Style.dateText, this.customStyles.dateText]}>{this.getDateStr()}</Text>
           </View>
           {this.showIcon && <Image
             style={[Style.dateIcon, this.customStyles.dateIcon]}
--- test/index.test.js
+++ test/index.test.js
@@ -222,21 +222,6 @@ describe('DatePicker:', () => {
     datePicker.mode = 'tttt';
     expect(datePicker.onPressDate).to.throw(Error);
   });
-  
-  it('getTitle - with placeholder', () => {
-    const placeholder = 'Please pick a date';
-    const wrapper = shallow(<DatePicker placeholder={placeholder} />);
-    const datePicker = wrapper.instance();
-
-    expect(datePicker.getTitle()).to.equal(placeholder);
-  });
-
-  it('getTitle - without placeholder', () => {
-    const wrapper = shallow(<DatePicker date="2016-06-04" />);
-    const datePicker = wrapper.instance();
-
-    expect(datePicker.getTitle()).to.equal(datePicker.getDateStr());
-  });
 });
 
 
