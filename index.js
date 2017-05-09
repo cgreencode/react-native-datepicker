1a1a0c64ea928a24cb6c2b20b34ad72248e3bc73
anproagile <anproagile@gmail.com>
2017-05-09 10:40:22 +0200

    Fix onDateChange on iOS

--- index.js
+++ index.js
@@ -29,13 +29,15 @@ class DatePicker extends Component {
     this.state = {
       date: this.getDate(),
       modalVisible: false,
-      animatedHeight: new Animated.Value(0)
+      animatedHeight: new Animated.Value(0),
+      allowPointerEvents: true
     };
 
     this.datePicked = this.datePicked.bind(this);
     this.onPressDate = this.onPressDate.bind(this);
     this.onPressCancel = this.onPressCancel.bind(this);
     this.onPressConfirm = this.onPressConfirm.bind(this);
+    this.onDateChange = this.onDateChange.bind(this);
     this.onDatePicked = this.onDatePicked.bind(this);
     this.onTimePicked = this.onTimePicked.bind(this);
     this.onDatetimePicked = this.onDatetimePicked.bind(this);
@@ -165,14 +167,25 @@ class DatePicker extends Component {
     return (<Text style={[Style.dateText, customStyles.dateText]}>{this.getDateStr()}</Text>);
   }
 
+  onDateChange(date) {
+    this.setState({
+      allowPointerEvents: false,
+      date: date
+    })
+    const timeoutId = setTimeout(() => {
+      this.setState({
+        allowPointerEvents: true
+      })
+      clearTimeout(timeoutId)
+    }, 500)
+  }
+
   onDatePicked({action, year, month, day}) {
     if (action !== DatePickerAndroid.dismissedAction) {
       this.setState({
         date: new Date(year, month, day)
       });
       this.datePicked();
-    } else {
-      this.onPressCancel();
     }
   }
 
@@ -182,8 +195,6 @@ class DatePicker extends Component {
         date: Moment().hour(hour).minute(minute).toDate()
       });
       this.datePicked();
-    } else {
-      this.onPressCancel();
     }
   }
 
@@ -199,8 +210,6 @@ class DatePicker extends Component {
         is24Hour: is24Hour,
         mode: androidMode
       }).then(this.onDatetimeTimePicked.bind(this, year, month, day));
-    } else {
-      this.onPressCancel();
     }
   }
 
@@ -210,8 +219,6 @@ class DatePicker extends Component {
         date: new Date(year, month, day, hour, minute)
       });
       this.datePicked();
-    } else {
-      this.onPressCancel();
     }
   }
 
@@ -325,16 +332,18 @@ class DatePicker extends Component {
                   <Animated.View
                     style={[Style.datePickerCon, {height: this.state.animatedHeight}, customStyles.datePickerCon]}
                   >
-                    <DatePickerIOS
-                      date={this.state.date}
-                      mode={mode}
-                      minimumDate={minDate && this.getDate(minDate)}
-                      maximumDate={maxDate && this.getDate(maxDate)}
-                      onDateChange={(date) => this.setState({date: date})}
-                      minuteInterval={minuteInterval}
-                      timeZoneOffsetInMinutes={timeZoneOffsetInMinutes}
-                      style={[Style.datePicker, customStyles.datePicker]}
-                    />
+                    <View pointerEvents={this.state.allowPointerEvents ? 'auto' : 'none'}>
+                      <DatePickerIOS
+                        date={this.state.date}
+                        mode={mode}
+                        minimumDate={minDate && this.getDate(minDate)}
+                        maximumDate={maxDate && this.getDate(maxDate)}
+                        onDateChange={this.onDateChange}
+                        minuteInterval={minuteInterval}
+                        timeZoneOffsetInMinutes={timeZoneOffsetInMinutes}
+                        style={[Style.datePicker, customStyles.datePicker]}
+                      />
+                    </View>
                     <TouchableHighlight
                       underlayColor={'transparent'}
                       onPress={this.onPressCancel}
