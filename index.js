c8787492f92f0263619862e48b097671a8f22985
anproagile <anproagile@gmail.com>
2017-05-10 11:40:18 +0800

    Update README.md

--- index.js
+++ index.js
@@ -257,29 +257,6 @@ class DatePicker extends Component {
       this.props.onOpenModal();
     }
   }
-  
-  _renderIcon() {
-    const {
-      showIcon,
-      iconSource,
-      iconComponent,
-      customStyles
-    } = this.props;
-
-    if (showIcon) {
-      if (!!iconComponent) {
-        return iconComponent;
-      }
-      return (
-        <Image
-          style={[Style.dateIcon, customStyles.dateIcon]}
-          source={iconSource}
-        />
-      );
-    }
-
-    return null;
-  }
 
   render() {
     const {
@@ -313,7 +290,10 @@ class DatePicker extends Component {
           <View style={dateInputStyle}>
             {this.getTitleElement()}
           </View>
-          {this._renderIcon()}
+          {showIcon && <Image
+            style={[Style.dateIcon, customStyles.dateIcon]}
+            source={iconSource}
+          />}
           {Platform.OS === 'ios' && <Modal
             transparent={true}
             animationType="none"
@@ -409,7 +389,6 @@ DatePicker.propTypes = {
   confirmBtnText: React.PropTypes.string,
   cancelBtnText: React.PropTypes.string,
   iconSource: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.object]),
-  iconComponent: React.PropTypes.element,
   customStyles: React.PropTypes.object,
   showIcon: React.PropTypes.bool,
   disabled: React.PropTypes.bool,
